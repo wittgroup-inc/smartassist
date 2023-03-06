f64bd68eb312933119e0c5bb9016d330f975e8f6
Pawan Gupta <pawankgupta.se@gmail.com>
2023-03-06 23:23:36 +0530

    Reply as stream.

--- smartassistlib/src/main/java/com/wittgroup/smartassistlib/datasources/ChatGpt.kt
+++ smartassistlib/src/main/java/com/wittgroup/smartassistlib/datasources/ChatGpt.kt
@@ -8,7 +8,6 @@ import com.wittgroup.smartassistlib.models.*
 import com.wittgroup.smartassistlib.network.ChatEventSourceListener
 import com.wittgroup.smartassistlib.network.ChatGptService
 import com.wittgroup.smartassistlib.network.NetworkHelper
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import okhttp3.MediaType.Companion.toMediaType
@@ -20,7 +19,6 @@ import okhttp3.sse.EventSource
 import okhttp3.sse.EventSourceListener
 import okhttp3.sse.EventSources
 import java.util.concurrent.TimeUnit
-import kotlin.time.milliseconds
 
 private const val DEFAULT_AI_MODEL = "text-davinci-003"
 private const val CHAT_DEFAULT_AI_MODEL = "gpt-3.5-turbo"
@@ -46,10 +44,10 @@ class ChatGpt(private val settingsDataSource: SettingsDataSource) : AiDataSource
 
     }
 
-    override suspend fun getAnswer(query: String): Resource<String> {
+    override suspend fun getAnswer(query: String): Resource<Flow<String>> {
         var model = settingsDataSource.getSelectedAiModel().successOr("")
         if (model.isEmpty()) {
-            model = "text-davinci-003"
+            model = DEFAULT_AI_MODEL
             settingsDataSource.chooseAiModel(model)
         }
         val result = MutableSharedFlow<String>(1)
@@ -58,7 +56,7 @@ class ChatGpt(private val settingsDataSource: SettingsDataSource) : AiDataSource
                 override fun onEvent(eventSource: EventSource, id: String?, type: String?, data: String) {
                     super.onEvent(eventSource, id, type, data)
                     if (data != "[DONE]") {
-                        val response = gson.fromJson(data, ChatResponse::class.java)
+                        val response = gson.fromJson(data, TextCompletionResponse::class.java)
                         result.tryEmit(response.choices[0].text)
 
                     }
@@ -77,7 +75,33 @@ class ChatGpt(private val settingsDataSource: SettingsDataSource) : AiDataSource
     }
 
     override suspend fun getReply(message: String): Resource<Flow<String>> {
-        TODO("Not yet implemented")
+        var model = settingsDataSource.getSelectedAiModel().successOr("")
+        if (model.isEmpty()) {
+            model = CHAT_DEFAULT_AI_MODEL
+            settingsDataSource.chooseAiModel(model)
+        }
+        val result = MutableSharedFlow<String>(1)
+        return try {
+            loadReply(message, object : ChatEventSourceListener() {
+                override fun onEvent(eventSource: EventSource, id: String?, type: String?, data: String) {
+                    super.onEvent(eventSource, id, type, data)
+                    if (data != "[DONE]") {
+                        val response = gson.fromJson(data, ChatCompletionStreamResponse::class.java)
+                        response.choices[0].delta.content?.let{
+                            result.tryEmit(it)
+                        }
+                    }
+                }
+
+                override fun onFailure(eventSource: EventSource, t: Throwable?, response: Response?) {
+                    super.onFailure(eventSource, t, response)
+                    Resource.Error(RuntimeException(response?.message))
+                }
+            })
+            Resource.Success(result)
+        } catch (e: Exception) {
+            Resource.Error(e)
+        }
     }
 
     companion object {
@@ -87,13 +111,28 @@ class ChatGpt(private val settingsDataSource: SettingsDataSource) : AiDataSource
 
     private fun load(query: String, listener: EventSourceListener) {
 
-
         val mediaType = "application/json; charset=utf-8".toMediaType()
-        val requestStr = gson.toJson(ChatRequest(model = "text-davinci-003", prompt = query, temperature = 0, maxTokens = 1000))
+        val requestStr = gson.toJson(TextCompletionRequest(model = DEFAULT_AI_MODEL, prompt = query, temperature = 0, maxTokens = MAX_TOKEN))
         val body = requestStr.toRequestBody(mediaType)
 
         val request = Request.Builder()
             .url("$BASE_URL$API_VERSION/completions")
+            .header("Authorization", "Bearer ${Constants.API_KEY}")
+            .addHeader("Content-Type", "application/json")
+            .addHeader("Accept", "text/event-stream")
+            .post(body)
+            .build()
+
+        EventSources.createFactory(client).newEventSource(request = request, listener = listener)
+    }
+
+    private fun loadReply(message: String, listener: EventSourceListener) {
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val requestStr = gson.toJson(ChatCompletionRequest(model = CHAT_DEFAULT_AI_MODEL, messages = listOf(Message(role = "user", content = message))))
+        val body = requestStr.toRequestBody(mediaType)
+
+        val request = Request.Builder()
+            .url("$BASE_URL$API_VERSION/chat/completions")
             .header("Authorization", "Bearer ${Constants.API_KEY}")
             .addHeader("Content-Type", "application/json")
             .addHeader("Accept", "text/event-stream")
