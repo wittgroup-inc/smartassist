168b108246ca08b07a54c5b9848132456510d146
Pawan Gupta <pawankgupta.se@gmail.com>
2024-01-27 14:14:41 +0530

    Changed Font.

--- app/src/main/java/com/gowittgroup/smartassist/ui/theme/Type.kt
+++ app/src/main/java/com/gowittgroup/smartassist/ui/theme/Type.kt
@@ -1,34 +1,43 @@
 package com.gowittgroup.smartassist.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.gowittgroup.smartassist.R
 
 // Set of Material typography styles to start with
+
+val OpenSans =
+    FontFamily(
+        Font(R.font.open_sans_regular),
+        Font(R.font.open_sans_italic, style = FontStyle.Italic),
+        Font(R.font.open_sans_medium, FontWeight.Medium),
+        Font(R.font.open_sans_medium_italic, FontWeight.Medium, style = FontStyle.Italic),
+        Font(R.font.open_sans_bold, FontWeight.Bold),
+        Font(R.font.open_sans_bold_italic, FontWeight.Bold, style = FontStyle.Italic),
+        Font(R.font.open_sans_light, FontWeight.Light),
+        Font(R.font.open_sans_light_italic, FontWeight.Light, style = FontStyle.Italic),
+        Font(R.font.open_sans_extra_bold, FontWeight.ExtraBold),
+        Font(R.font.open_sans_extra_bold, FontWeight.ExtraBold, style = FontStyle.Italic)
+    )
+
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge = Typography().displayLarge.copy(fontFamily = OpenSans),
+    displayMedium = Typography().displayMedium.copy(fontFamily = OpenSans),
+    displaySmall = Typography().displaySmall.copy(fontFamily = OpenSans),
+    headlineLarge = Typography().headlineLarge.copy(fontFamily = OpenSans),
+    headlineMedium = Typography().headlineMedium.copy(fontFamily = OpenSans),
+    headlineSmall = Typography().headlineSmall.copy(fontFamily = OpenSans),
+    titleLarge = Typography().titleLarge.copy(fontFamily = OpenSans),
+    titleMedium = Typography().titleMedium.copy(fontFamily = OpenSans),
+    titleSmall = Typography().titleSmall.copy(fontFamily = OpenSans),
+    bodyLarge = Typography().bodyLarge.copy(fontFamily = OpenSans),
+    bodyMedium = Typography().bodyMedium.copy(fontFamily = OpenSans),
+    bodySmall = Typography().bodySmall.copy(fontFamily = OpenSans),
+    labelLarge = Typography().labelLarge.copy(fontFamily = OpenSans),
+    labelMedium = Typography().labelMedium.copy(fontFamily = OpenSans),
+    labelSmall = Typography().labelSmall.copy(fontFamily = OpenSans),
 )
