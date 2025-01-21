9ebc8cfc0b0ec00da4b5b72dd043aec8bc0813b5
Pawan Gupta <pawankgupta.se@gmail.com>
2025-01-22 00:11:00 +0530

    Fixed gemini

--- smartassistlib/src/main/java/com/gowittgroup/smartassistlib/util/Constants.kt
+++ smartassistlib/src/main/java/com/gowittgroup/smartassistlib/util/Constants.kt
@@ -1,9 +1,12 @@
 package com.gowittgroup.smartassistlib.util
 
+import com.gowittgroup.smartassistlib.models.ai.AiTools
+
 object Constants {
     const val CHAT_GPT_API_VERSION = "v1"
     const val CHAT_GPT_BASE_URL = "https://api.openai.com/"
     const val CHAT_GPT_DEFAULT_CHAT_AI_MODEL = "gpt-4o-mini"
+    val DEFAULT_AI_TOOL = AiTools.GEMINI
     const val GEMINI_DEFAULT_CHAT_AI_MODEL = "gemini-1.5-flash"
     const val USER_COLLECTION_PATH = "users"
     object UserDataKey{
--- smartassistlib/src/main/java/com/gowittgroup/smartassistlib/util/DataSourceProvider.kt
+++ smartassistlib/src/main/java/com/gowittgroup/smartassistlib/util/DataSourceProvider.kt
@@ -7,6 +7,7 @@ import com.gowittgroup.smartassistlib.data.di.CHAT_GPT
 import com.gowittgroup.smartassistlib.data.di.GEMINI
 import com.gowittgroup.smartassistlib.domain.models.successOr
 import com.gowittgroup.smartassistlib.models.ai.AiTools
+import com.gowittgroup.smartassistlib.util.Constants.DEFAULT_AI_TOOL
 import javax.inject.Inject
 import javax.inject.Named
 import javax.inject.Singleton
@@ -16,8 +17,8 @@ class AiDataSourceProvider @Inject constructor(@Named(CHAT_GPT) private val chat
                                                @Named(GEMINI) private val gemini: AiDataSource, private val settingsDataSource: SettingsDataSource
 ) {
      suspend fun getDataSource(): AiDataSource =
-        when(settingsDataSource.getSelectedAiTool().successOr(AiTools.CHAT_GPT)){
-            AiTools.NONE -> chatGpt
+        when(settingsDataSource.getSelectedAiTool().successOr(DEFAULT_AI_TOOL)){
+            AiTools.NONE -> gemini
             AiTools.CHAT_GPT ->  chatGpt
             AiTools.GEMINI ->  gemini
         }
